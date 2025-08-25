b8f02d9fb34ed9fe2ed01cf7f856478c95d5ba45
Dev Tuppa <165136974+TupinangeJoao@users.noreply.github.com>
2025-08-25 00:56:26 -0300

    Novas aulas

---
+++ Aula 11 condições/exe008.js
@@ -0,0 +1,6 @@
+var vel = 65.6
+
+if  (vel > 60){
+    console.log('Você está acima da velocidade permitida, MULTADO!!!')
+}
+console.log('Dirija com Cinto de Segurança!')
---
+++ Aula 11 condições/exe009.js
@@ -0,0 +1,11 @@
+var pais = 'Brasil'
+
+if  (pais == 'Brasil'){
+    console.log('Você é brasileiro')
+}
+else {
+        console.log('Você infelizmente não pode se inscrever neste curso!')
+    }
+
+
+console.log('Cuida!!!')
